e65f68efbaed1dd93e0b3395f4cd4b876436597e
Drew Wilenzick <ddw77@cornell.edu>
2023-12-06 23:16:38 -0500

    Modified test suite to start testing User.ml, functionality for the stock portfolio module

--- test/main.ml
+++ test/main.ml
@@ -1,4 +1,16 @@
 open OUnit2
+open User
 
-let suite = "scraping test suite" >::: []
+let tsla = make_stock "TSLA" 100.0
+
+let suite =
+  "scraping test suite"
+  >::: [
+         ( "Empty Test" >:: fun _ ->
+           assert_bool "failing test" (User.is_empty empty) );
+         ( "Empty Test Non - empty" >:: fun _ ->
+           assert_bool "failing test" (not (User.is_empty (buy tsla 100 empty)))
+         );
+       ]
+
 let _ = run_test_tt_main suite
