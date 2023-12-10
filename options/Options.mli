1b7bea16c8c8250235abebe97f067d7529824d8f
ael254 <ael254@cornell.edu>
2023-12-10 10:18:54 -0500

    Minor

--- options/Options.mli
+++ options/Options.mli
@@ -4,7 +4,6 @@ type option_type =
   | Call
   | Put
 
-(* Define a record type for representing an options contract *)
 type option_contract = {
   underlying_price : float;
   strike_price : float;
@@ -14,10 +13,12 @@ type option_contract = {
   option_type : option_type;
   steps : int;
 }
+(** Define a record type for representing an options contract *)
 
-(* Functions to nicely print aspects of an option, except for steps which is
-   hidden from user *)
 val print_underlying_price : option_contract -> unit
+(** Functions to nicely print aspects of an option, except for steps which is
+    hidden from user *)
+
 val print_strike_price : option_contract -> unit
 val print_time_to_expiry : option_contract -> unit
 val print_volatility : option_contract -> unit
@@ -35,35 +36,36 @@ val create_contract :
   int ->
   option_contract
 
-(* Updates the options contract based on factors that change over time so its
-   current value can be found *)
 val update_contract :
   option_contract -> float -> float -> float -> float -> option_contract
+(** Updates the options contract based on factors that change over time so its
+    current value can be found *)
 
-(* Returns a boolean of whether the contract should be executed or not, true if
-   it should and false if not*)
 val execute_contract : option_contract -> bool
+(** Returns a boolean of whether the contract should be executed or not, true if
+    it should and false if not*)
 
-(* Represents the cumulative distribution function for a standard normal
-   variable *)
 val cdf : float -> float
+(** Represents the cumulative distribution function for a standard normal
+    variable *)
 
-(* Represents the probability distribution function for a standard normal
-   variable *)
 val pdf : float -> float
+(** Represents the probability distribution function for a standard normal
+    variable *)
 
-(* Black-Scholes formula for European call option pricing *)
 val black_scholes_price : option_contract -> float
+(** Black-Scholes formula for European call option pricing *)
 
-(* Greeks: Delta, Gamma, Theta, Vega, Rho *)
 val delta : option_contract -> float
+(** Greeks: Delta, Gamma, Theta, Vega, Rho *)
+
 val gamma : option_contract -> float
 val theta : option_contract -> float
 val vega : option_contract -> float
 val rho : option_contract -> float
 
-(* Implied volatility calculation using Newton's method *)
 val implied_volatility : option_contract -> float -> float
+(** Implied volatility calculation using Newton's method *)
 
-(* Binomial method for call option pricing*)
 val binomial_price : option_contract -> float
+(** Binomial method for call option pricing*)
